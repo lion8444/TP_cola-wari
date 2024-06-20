25b3057089662840c4a0f9fd153f0d5b57ef10a8
IF <if1396443@gmail.com>
2024-06-20 11:01:55 +0900

    pullされるやつ

---
+++ cola_wari/src/main/java/jp/co/sss/management/repository/AgendaEntryRepository.java
@@ -0,0 +1,29 @@
+package jp.co.sss.management.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sss.management.entity.AgendaEntry;
+
+/**
+ * usersテーブル用リポジトリ
+ *
+ * @author System Shared
+ */
+@Repository
+public interface AgendaEntryRepository extends JpaRepository<AgendaEntry, Integer> {
+
+	List<AgendaEntry> findByAgendaStatus(int status);
+
+	@Query("SELECT ae FROM AgendaEntry ae WHERE ae.user.userId = :userId and ae.agenda.status = :status")
+	List<AgendaEntry> findByUserId(@Param("userId") Integer userId, @Param("status") int status);
+
+	//@Query("SELECT ae " +
+	//	"FROM AgendaEntry ae " +
+	//	"WHERE user.userId =: userId")
+	//List<AgendaEntry> findByAgendaStatus(@Param("userId") Integer userId);
+}
