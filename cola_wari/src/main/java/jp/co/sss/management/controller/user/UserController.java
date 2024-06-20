c16314eee14a67d3b00dbb89c306511aa9bd71a7
IF <if1396443@gmail.com>
2024-06-20 11:54:56 +0900

    240620 userController

--- cola_wari/src/main/java/jp/co/sss/management/controller/user/UserController.java
+++ cola_wari/src/main/java/jp/co/sss/management/controller/user/UserController.java
@@ -1,16 +1,52 @@
 package jp.co.sss.management.controller.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.persistence.EntityManager;
+import jp.co.sss.management.entity.AgendaEntry;
+import jp.co.sss.management.entity.User;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
-    
-    @GetMapping("list")
-    public String showUsers(Model model) {
-        return "user/users";
-    }
+
+	@Autowired
+	jp.co.sss.management.repository.UserRepository userRepository;
+	
+	@Autowired
+	jp.co.sss.management.repository.AgendaEntryRepository agendaEntryRepository;
+	
+	@Autowired
+	EntityManager entityManager;
+	
+	@GetMapping({ "/user/list" })
+	public String userList(Model model) {
+		List<User> usersList = userRepository.findByStatus(0);
+		model.addAttribute("users", usersList);
+
+		return "user/list";
+	}
+	
+	@GetMapping({"/user/detail/{id}"})
+	@PostMapping({"/user/detail/{id}"})
+	public String userDetail(Model model,@PathVariable Integer id) {
+		User user = userRepository.findByUserId(id);
+		model.addAttribute("users",user);
+		System.out.println(model.getAttribute("users"));
+		List<AgendaEntry> progressEntry = agendaEntryRepository.findByUserId(id,0);
+		model.addAttribute("progresses",progressEntry);
+		List<AgendaEntry> endEntry = agendaEntryRepository.findByUserId(id,1);
+		model.addAttribute("ends",endEntry);
+		System.out.println("endの処理できてます！！");
+		
+		return "user/detail";
+	}
+	
 }
+
